d79d4b7cbad042fd164fc80afbb59d112fc5112f
Jorge Pando Jimeno <jorge.pando@babelgroup.com>
2022-03-10 14:02:52 +0100

    Primeros pasos Login Web

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.naming.ldap.Rdn;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.entidad.Usuario;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		String password = request.getParameter("password");
+		
+		Usuario u = new Usuario();
+		u.setNombre(nombre);
+		u.setPassword(password);
+		
+		String nomUsuario1 = "JorgePJ13";
+		String nomUsuario2 = "JordiPlayer55";
+		String nomUsuario3 = "JorgeMatamoscas";
+		
+		RequestDispatcher rd = request.getRequestDispatcher("ResultadoLogin.jsp");
+		request.setAttribute("info", u);
+		request.getSession().setAttribute("nombre", nombre);
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/ResultadoLogin.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Información Usuario</title>
+		<link rel="stylesheet" href="css/styleResultadoLogin.css">
+	</head>
+	<body>
+		<header>Bienvenido de nuevo, ${param.nombre}</header>
+		<p>Información de cuenta: ${info}</p>
+	</body>
+</html>
